8e11693cbfdbd2f65eec6d2fa9e0a2cb2690d0a8
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-03-06 16:33:49 +0000

    initial checkin, but not ready yet
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.site/trunk@61035 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ setup.py
@@ -0,0 +1,65 @@
+import codecs
+from setuptools import setup, find_packages
+
+VERSION = '0.0.0'
+
+entry_points = {
+	'console_scripts': [
+	],
+}
+
+TESTS_REQUIRE = [
+	'fudge',
+	'nose',
+	'nose-timer',
+	'nose-pudb',
+	'nose-progressive',
+	'nose2[coverage_plugin]',
+	'pyhamcrest',
+	'nti.nose_traceback_info',
+	'nti.testing'
+]
+
+setup(
+	name='nti.site',
+	version=VERSION,
+	author='Jason Madden',
+	author_email='jason@nextthought.com',
+	description="NTI Site",
+	long_description=codecs.open('README.rst', encoding='utf-8').read(),
+	license='Proprietary',
+	keywords='Site management',
+	classifiers=[
+		'Intended Audience :: Developers',
+		'Natural Language :: English',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Python :: Implementation :: CPython'
+	],
+	packages=find_packages('src'),
+	package_dir={'': 'src'},
+	namespace_packages=['nti'],
+	tests_require=TESTS_REQUIRE,
+	install_requires=[
+		'setuptools',
+        'persistent',
+        'ZODB',
+		'zope.component',
+        'zope.container',
+        'zope.interface',
+        'zope.location',
+        'zope.proxy',
+		'zope.security',
+        'zope.site',
+        'zope.traversing',
+        'nti.schema'
+	],
+	extras_require={
+		'test': TESTS_REQUIRE,
+	},
+	dependency_links=[
+		'git+https://github.com/NextThought/nti.nose_traceback_info.git#egg=nti.nose_traceback_info'
+	],
+	entry_points=entry_points
+)
