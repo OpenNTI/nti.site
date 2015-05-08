de48fd33616d7023060e8dc1c373b32cd5ca64d5
Carlos Sanchez <carlos.sanchez@nextthought.com>
2015-05-08 02:48:07 +0000

    add utils module
    git-svn-id: https://repos.nextthought.com/svn/nti-svn/nti.site/trunk@64750 3dfb626b-079f-4ab1-a9f6-4654e5377508

---
+++ src/nti/site/utils.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, unicode_literals, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+from .interfaces import IHostPolicySiteManager
+
+def registerUtility(registry, component, provided, name):
+	if IHostPolicySiteManager.providedBy(registry):
+		return registry.subscribedRegisterUtility(component,
+									 			  provided=provided,
+									 			  name=name)
+	else:
+		return registry.registerUtility(component,
+									 	provided=provided,
+									 	name=name)
+
+def unregisterUtility(registry, provided, name):
+	if IHostPolicySiteManager.providedBy(registry):
+		return registry.subscribedUnregisterUtility(provided=provided, name=name)
+	else:
+		return registry.unregisterUtility(provided=provided, name=name)
