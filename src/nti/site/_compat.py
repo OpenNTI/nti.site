f0992b0622966b543f26975e70cecd583ca4c03e
Joshua Zuech <jzuech@jzuech.local>
2017-09-15 09:02:35 -0500

    Add missing files

---
+++ src/nti/site/_compat.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import print_function, absolute_import, division
+__docformat__ = "restructuredtext en"
+
+logger = __import__('logging').getLogger(__name__)
+
+import sys
+
+if sys.version_info[0] == 3:
+    def _unicode(s): return str(s)
+else:
+    _unicode = unicode
+
+
+def text_(s, encoding='utf-8', err='strict'):
+    """
+    Decode a byte sequence and unicode result
+    """
+    s = s.decode(encoding, err) if isinstance(s, bytes) else s
+    return _unicode(s) if s is not None else None
